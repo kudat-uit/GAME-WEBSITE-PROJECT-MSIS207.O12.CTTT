a36a6dbce5a50fdb76bed9dc5ee35526b6321a60
Nguyen Nhat Thuong <thuongngnh@Nguyens-MacBook-Pro.local>
2023-11-17 23:35:11 +0700

    upload homepage

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// Import component
+import InfoHome from "./assets/components/InfoHome/InfoHome.jsx"
+
+function App(){
+    return <div>
+        <InfoHome/>
+    </div>
+}
+
+
+export default App;
